452201273e825750f46900b296a2229e9b76aea5
alom4hdi <alom4hdi@gmail.com>
2023-06-23 00:04:32 +0330

    Added  types

--- ext/types.v
+++ ext/types.v
@@ -2,6 +2,9 @@ module ext
 
 import globals
 
+pub type ResponseMessage = fn (Bot, Message)
+pub type ResponseCallbackQuery = fn (Bot, CallbackQuery)
+
 pub struct Bot {
 pub:
 	bot_token string
@@ -21,10 +24,6 @@ pub:
 	description string
 }
 
-type ChatMember = ChatMemberOwner | ChatMemberAdministrator | ChatMemberMember | ChatMemberRestricted | ChatMemberLeft | ChatMemberBanned
-pub type ResponseMessage = fn (Bot, Message)
-pub type ResponseCallbackQuery = fn (Bot, CallbackQuery)
-
 pub struct Updates {
 pub:
 	update_id i64
@@ -42,6 +41,53 @@ pub:
 	my_chat_member ChatMemberUpdated
 	chat_member ChatMemberUpdated
 	chat_join_request ChatJoinRequest
+}
+
+pub struct User {
+pub:
+	id i64
+	is_bot bool
+	first_name string
+	last_name string
+	username string
+	language_code string
+	is_premium bool = true
+	added_to_attachement_menu bool = true
+	can_join_groups bool
+	can_read_all_group_messages bool
+	support_inline_queries bool
+}
+
+pub struct Chat {
+pub:
+	id i64
+	@type string
+	title string
+	username string
+	first_name string
+	last_name string
+	is_forum bool = true
+	photo ChatPhoto
+	active_usernames []string
+	emoji_status_custom_emoji_id string
+	bio string
+	has_private_forwards bool = true
+	has_restricted_voice_and_video_messages bool = true
+	join_to_send_messages bool = true
+	join_by_request bool = true
+	description string
+	invite_link string
+	pinned_message &Message = unsafe { nil }
+	permissions ChatPermissions
+	slow_mode_delay int
+	message_auto_delete_time int
+	has_aggressive_anti_spam_enabled bool = true
+	has_hidden_members bool = true
+	has_protected_content bool = true
+	sticker_set_name string
+	can_set_sticker_set bool = true
+	linked_chat_id i64
+	location ChatLocation
 }
 
 pub struct Message {
@@ -119,75 +165,9 @@ pub:
 	reply_markup InlineKeyboardMarkup
 }
 
-pub struct User {
+pub struct MessageId {
 pub:
-	id i64
-	is_bot bool
-	first_name string
-	last_name string
-	username string
-	language_code string
-	is_premium bool = true
-	added_to_attachement_menu bool = true
-	can_join_groups bool
-	can_read_all_group_messages bool
-	support_inline_queries bool
-}
-
-pub struct Chat {
-pub:
-	id i64
-	@type string
-	title string
-	username string
-	first_name string
-	last_name string
-	is_forum bool = true
-	photo ChatPhoto
-	active_usernames []string
-	emoji_status_custom_emoji_id string
-	bio string
-	has_private_forwards bool = true
-	has_restricted_voice_and_video_messages bool = true
-	join_to_send_messages bool = true
-	join_by_request bool = true
-	description string
-	invite_link string
-	pinned_message &Message = unsafe { nil }
-	permissions ChatPermissions
-	slow_mode_delay int
-	message_auto_delete_time int
-	has_aggressive_anti_spam_enabled bool = true
-	has_hidden_members bool = true
-	has_protected_content bool = true
-	sticker_set_name string
-	can_set_sticker_set bool = true
-	linked_chat_id i64
-	location ChatLocation
-}
-
-pub struct ChatPermissions {
-pub:
-	can_send_messages bool
-	can_send_audios bool
-	can_send_documents bool
-	can_send_photos bool
-	can_send_videos bool
-	can_send_video_notes bool
-	can_send_voice_notes bool
-	can_send_polls bool
-	can_send_other_messages bool
-	can_add_web_page_previews bool
-	can_change_info bool
-	can_invite_users bool
-	can_pin_messages bool
-	can_manage_topics bool
-}
-
-pub struct ChatLocation {
-pub:
-	location Location
-	address string
+	message_id i64
 }
 
 pub struct MessageEntity {
@@ -201,6 +181,15 @@ pub:
 	custom_emoji_id string
 }
 
+pub struct PhotoSize {
+pub:
+	file_id string
+	file_unique_id string
+	width int
+	height int
+	file_size int
+}
+
 pub struct Animation {
 pub:
 	file_id string
@@ -212,23 +201,6 @@ pub:
 	file_name string
 	mime_type string
 	file_size int
-}
-
-pub struct PhotoSize {
-pub:
-	file_id string
-	file_unique_id string
-	width int
-	height int
-	file_size int
-}
-
-pub struct ChatPhoto {
-pub:
-	small_file_id string
-	small_file_unique_id string
-	big_file_id string
-	big_file_unique_id string
 }
 
 pub struct Audio {
@@ -352,47 +324,42 @@ pub:
 	google_place_type string
 }
 
-pub struct MessageAutoDeleteTimerChanged {
+pub struct WebAppData {
 pub:
-	message_auto_delete_time int
+	data string
+	button_text string
 }
 
-pub struct Invoice {
+pub struct ProximityAlertTriggered {
 pub:
-	title string
-	description string
-	start_parameter string
-	currency string
-	total_amount string
+	traveler User
+	watcher User
+	distance int
 }
 
-pub struct SuccessfulPayment {
-pub:
-	currency string
-	total_amount int
-	invoice_payload string
-	shipping_option_id string
-	order_info OrderInfo
-	telegram_payment_charge_id string
-	provider_payment_charge_id string
-}
-
-pub struct OrderInfo {
+pub struct ForumTopicCreated {
 pub:
 	name string
-	phone_number string
-	email string
-	shipping_address ShippingAddress
+	icon_color int
+	icon_custom_emoji_id string
 }
 
-pub struct ShippingAddress {
+pub struct ForumTopicClosed {
+}
+
+pub struct ForumTopicEdited {
 pub:
-	country_code string
-	state string
-	city string
-	street_line1 string
-	street_line2 string
-	post_code string
+	name string
+	icon_custom_emoji_id string
+}
+
+pub struct ForumTopicReopened {
+}
+
+pub struct GeneralForumTopicHidden {
+}
+
+pub struct GeneralForumTopicUnhidden {
 }
 
 pub struct UserShared {
@@ -410,73 +377,6 @@ pub:
 pub struct WriteAccessAllowed {
 pub:
 	web_app_name string
-}
-
-pub struct PassportData {
-pub:
-	data []EncryptedPassportElement
-	credentials EncryptedCredentials
-}
-
-pub struct EncryptedPassportElement {
-pub:
-	@type string
-	date string
-	phone_number string
-	email string
-	files []PassportFile
-	front_side PassportFile
-	reverse_side PassportFile
-	selfie PassportFile
-	translation []PassportFile
-	hash string
-}
-
-pub struct PassportFile {
-pub:
-	file_id string
-	file_unique_id string
-	file_size int
-	file_date int
-}
-
-pub struct EncryptedCredentials {
-pub:
-	data string
-	hash string
-	secret string
-}
-
-pub struct ProximityAlertTriggered {
-pub:
-	traveler User
-	watcher User
-	distance int
-}
-
-pub struct ForumTopicCreated {
-pub:
-	name string
-	icon_color int
-	icon_custom_emoji_id string
-}
-
-pub struct ForumTopicEdited {
-pub:
-	name string
-	icon_custom_emoji_id string
-}
-
-pub struct ForumTopicClosed {
-}
-
-pub struct ForumTopicReopened {
-}
-
-pub struct GeneralForumTopicHidden {
-}
-
-pub struct GeneralForumTopicUnhidden {
 }
 
 pub struct VideoChatScheduled {
@@ -497,10 +397,74 @@ pub:
 	users []User
 }
 
-pub struct WebAppData {
+pub struct UserProfilePhotos {
 pub:
-	data string
-	button_text string
+	total_count i64
+	photos [][]PhotoSize
+}
+
+pub struct File {
+pub:
+	file_id string
+	file_unique_id string
+	file_size int
+	file_path string
+}
+
+pub struct WebAppInfo {
+pub:
+	url string
+}
+
+pub struct ReplyKeyboardMarkup {
+pub:
+	keyboard [][]KeyboardButton
+	is_persistent bool
+	resize_keyboard bool
+	one_time_keyboard bool
+	input_field_placeholder string
+	selective bool
+}
+
+pub struct KeyboardButton {
+pub:
+	text string
+	request_user KeyboardButtonRequestUser
+	request_chat KeyboardButtonRequestChat
+	request_contact bool
+	request_location bool
+	request_poll KeyboardButtonPollType
+	web_app WebAppInfo
+}
+
+pub struct KeyboardButtonRequestUser {
+pub:
+	request_id i64
+	user_is_bot bool
+	user_is_premium bool
+}
+
+pub struct KeyboardButtonRequestChat {
+pub:
+	request_id i64
+	chat_is_channel bool
+	chat_is_forum bool
+	chat_has_username bool
+	chat_is_created bool
+	user_administrator_rights ChatAdministratorRights
+	bot_administrator_rights ChatAdministratorRights
+	bot_is_member bool
+}
+
+pub struct KeyboardButtonPollType {
+pub:
+	@type string
+}
+
+pub struct ReplyKeyboardRemove {
+pub:
+	remove_keyboard bool = true
+	selective bool
 }
 
 pub struct InlineKeyboardMarkup {
@@ -522,11 +486,6 @@ pub:
 	pay bool
 }
 
-pub struct WebAppInfo {
-pub:
-	url string
-}
-
 pub struct LoginUrl {
 pub:
 	url string
@@ -544,17 +503,6 @@ pub:
 	allow_channel_chats bool
 }
 
-pub struct CallbackGame {
-}
-
-pub struct InlineQuery {
-
-}
-
-pub struct ChosenInlineResult {
-
-}
-
 pub struct CallbackQuery {
 pub:
 	id string
@@ -566,24 +514,51 @@ pub:
 	game_short_name string
 }
 
-pub struct ShippingQuery {
-
-}
-
-pub struct PreCheckoutQuery {
-
-}
-
-pub struct ChatMemberUpdated {
+pub struct ForceReply {
 pub:
-	chat Chat
-	from User
-	date int
-	old_chat_member ChatMember
-	new_chat_member ChatMember
-	invite_link ChatInviteLink
-	via_chat_folder_invite_link bool
+	force_reply bool = true
+	input_field_placeholder string
+	selective bool
 }
+
+pub struct ChatPhoto {
+pub:
+	small_file_id string
+	small_file_unique_id string
+	big_file_id string
+	big_file_unique_id string
+}
+
+pub struct ChatInviteLink {
+pub:
+	invite_link string
+	creator User
+	creates_join_request bool
+	is_primary bool
+	is_revoked bool
+	name string
+	expire_date int
+	member_limit int
+	pending_join_request_count int
+}
+
+pub struct ChatAdministratorRights {
+pub:
+	is_anonymous bool
+	can_manage_chat bool
+	can_delete_messages bool
+	can_manage_video_chats bool
+	can_restrict_members bool
+	can_promote_members bool
+	can_change_info bool
+	can_invite_users bool
+	can_post_messages bool
+	can_edit_messages bool
+	can_pin_messages bool
+	can_manage_topics bool
+}
+
+pub type ChatMember = ChatMemberOwner | ChatMemberAdministrator | ChatMemberMember | ChatMemberRestricted | ChatMemberLeft | ChatMemberBanned
 
 pub struct ChatMemberOwner  {
 pub:
@@ -654,6 +629,17 @@ pub:
 	until_date int
 }
 
+pub struct ChatMemberUpdated {
+pub:
+	chat Chat
+	from User
+	date int
+	old_chat_member ChatMember
+	new_chat_member ChatMember
+	invite_link ChatInviteLink
+	via_chat_folder_invite_link bool
+}
+
 pub struct ChatJoinRequest {
 pub:
 	chat Chat
@@ -664,17 +650,309 @@ pub:
 	invite_link ChatInviteLink
 }
 
-pub struct ChatInviteLink {
+pub struct ChatPermissions {
 pub:
-	invite_link string
-	creator User
-	creates_join_request bool
-	is_primary bool
-	is_revoked bool
+	can_send_messages bool
+	can_send_audios bool
+	can_send_documents bool
+	can_send_photos bool
+	can_send_videos bool
+	can_send_video_notes bool
+	can_send_voice_notes bool
+	can_send_polls bool
+	can_send_other_messages bool
+	can_add_web_page_previews bool
+	can_change_info bool
+	can_invite_users bool
+	can_pin_messages bool
+	can_manage_topics bool
+}
+
+pub struct ChatLocation {
+pub:
+	location Location
+	address string
+}
+
+pub struct ForumTopic {
+pub:
+	message_thread_id i64
 	name string
-	expire_date int
-	member_limit int
-	pending_join_request_count int
+	icon_color i64
+	icon_custom_emoji_id string
+}
+
+pub struct BotCommand {
+pub:
+	command string
+	description string
+}
+
+pub type BotCommandScope = BotCommandScopeDefault | BotCommandScopeAllPrivateChats | BotCommandScopeAllGroupChats | BotCommandScopeAllChatAdministrators | BotCommandScopeChat | BotCommandScopeChatAdministrators | BotCommandScopeChatMember
+
+pub struct BotCommandScopeDefault {
+pub:
+	@type string
+}
+
+pub struct BotCommandScopeAllPrivateChats {
+pub:
+	@type string
+}
+
+pub struct BotCommandScopeAllGroupChats {
+pub:
+	@type string
+}
+
+pub struct BotCommandScopeAllChatAdministrators {
+pub:
+	@type string
+}
+
+pub struct BotCommandScopeChat {
+pub:
+	@type string
+	chat_id i64
+}
+
+pub struct BotCommandScopeChatAdministrators {
+pub:
+	@type string
+	chat_id i64
+}
+
+pub struct BotCommandScopeChatMember {
+pub:
+	@type string
+	chat_id i64
+	user_id i64
+}
+
+pub struct BotName {
+pub:
+	name string
+}
+
+pub struct BotDescription {
+pub:
+	description string
+}
+
+pub struct BotShortDescription {
+pub:
+	short_description string
+}
+
+pub type MenuButton = MenuButtonCommands | MenuButtonWebApp | MenuButtonDefault
+
+pub struct MenuButtonCommands {
+pub:
+	@type string
+}
+
+pub struct MenuButtonWebApp {
+pub:
+	@type string
+	text string
+	web_app WebAppInfo
+}
+
+pub struct MenuButtonDefault {
+pub:
+	@type string
+}
+
+pub struct ResponseParameters {
+pub:
+	migrate_to_chat_id i64
+	retry_after i64
+}
+
+pub type InputMedia = InputMediaAnimation | InputMediaDocument | InputMediaAudio | InputMediaPhoto | InputMediaVideo
+
+pub struct InputMediaPhoto {
+pub:
+	@type string
+	media string
+	caption string
+	parse_mode string
+	caption_entities []MessageEntity
+	has_spoiler bool
+}
+
+pub type FileString = InputFile | string
+
+pub struct InputMediaVideo {
+pub:
+	@type string
+	media string
+	thumbnail FileString
+	caption string
+	parse_mode string
+	caption_entities []MessageEntity
+	width i64
+	height i64
+	duration i64
+	support_streaming bool
+	has_spoiler bool
+}
+
+pub struct InputMediaAnimation {
+pub:
+	@type string
+	media string
+	thumbnail FileString
+	caption string
+	parse_mode string
+	caption_entities []MessageEntity
+	width i64
+	height i64
+	duration i64
+	support_streaming bool
+	has_spoiler bool
+}
+
+pub struct InputMediaAudio {
+pub:
+	@type string
+	media string
+	thumbnail FileString
+	caption string
+	parse_mode string
+	caption_entities []MessageEntity
+	duration i64
+	performer string
+	title string
+}
+
+pub struct InputMediaDocument {
+pub:
+	@type string
+	media string
+	thumbnail FileString
+	caption string
+	parse_mode string
+	caption_entities []MessageEntity
+	disable_content_type_detection bool
+}
+
+pub struct MessageAutoDeleteTimerChanged {
+pub:
+	message_auto_delete_time int
+}
+
+pub struct Invoice {
+pub:
+	title string
+	description string
+	start_parameter string
+	currency string
+	total_amount string
+}
+
+pub struct SuccessfulPayment {
+pub:
+	currency string
+	total_amount int
+	invoice_payload string
+	shipping_option_id string
+	order_info OrderInfo
+	telegram_payment_charge_id string
+	provider_payment_charge_id string
+}
+
+pub struct OrderInfo {
+pub:
+	name string
+	phone_number string
+	email string
+	shipping_address ShippingAddress
+}
+
+pub struct ShippingAddress {
+pub:
+	country_code string
+	state string
+	city string
+	street_line1 string
+	street_line2 string
+	post_code string
+}
+
+pub struct PassportData {
+pub:
+	data []EncryptedPassportElement
+	credentials EncryptedCredentials
+}
+
+pub struct EncryptedPassportElement {
+pub:
+	@type string
+	date string
+	phone_number string
+	email string
+	files []PassportFile
+	front_side PassportFile
+	reverse_side PassportFile
+	selfie PassportFile
+	translation []PassportFile
+	hash string
+}
+
+pub struct PassportFile {
+pub:
+	file_id string
+	file_unique_id string
+	file_size int
+	file_date int
+}
+
+pub struct EncryptedCredentials {
+pub:
+	data string
+	hash string
+	secret string
+}
+
+pub struct CallbackGame {
+}
+
+pub struct InlineQuery {
+	id string
+	from User
+	query string
+	offset string
+	chat_type string
+	location Location
+}
+
+pub struct ChosenInlineResult {
+pub:
+	result_id string
+	from User
+	location Location
+	inline_message_id string
+	query string
+}
+
+pub struct ShippingQuery {
+pub:
+	id string
+	from User
+	invoice_payload string
+	shipping_address ShippingAddress
+}
+
+pub struct PreCheckoutQuery {
+pub:
+	id string
+	from User
+	currency string
+	total_amount i64
+	invoice_payload string
+	shipping_option_id string
+	order_info OrderInfo
 }
 
 pub struct Sticker {
@@ -694,14 +972,6 @@ pub:
 	custom_emoji_id string
 	needs_repainting bool = true
 	file_size int
-}
-
-pub struct File {
-pub:
-	file_id string
-	file_unique_id string
-	file_size int
-	file_path string
 }
 
 pub struct MarkPosition {
